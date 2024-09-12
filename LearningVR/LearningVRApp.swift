633a1d97786fc44c35dabb664043e20cdf1ccad0
Divyansh-Kaushik84 <84divyanshkaushik@gmail.com>
2024-09-13 04:50:06 +0530

    Initial Commit

---
+++ LearningVR/LearningVRApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningVRApp.swift
+//  LearningVR
+//
+//  Created by Divyansh Kaushik on 13/09/24.
+//
+
+import SwiftUI
+
+@main
+struct LearningVRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
